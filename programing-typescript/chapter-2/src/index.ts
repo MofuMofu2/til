f208c7be2816d6129e7aa7899e31e02f31edef96
MofuMofu2 <froakie002@gmail.com>
2020-09-29 17:12:41 +0900

    [fix] 再代入していないのでlet -> constに変更

--- programing-typescript/chapter-2/src/index.ts
+++ programing-typescript/chapter-2/src/index.ts
@@ -2,10 +2,10 @@ console.log("Hello TypeScript!");
 
 // 2.5 練習問題
 
-let a = 1 + 2;
-let b = a + 3;
-let c = {
+const a = 1 + 2;
+const b = a + 3;
+const c = {
   apple: a,
   banana: b,
 };
-let d = c.apple * 4;
+const d = c.apple * 4;

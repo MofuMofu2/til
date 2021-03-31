0da783c83b50c93ab1b1ebad6a9da38a02d833e4
MofuMofu <froakie002@gmail.com>
2021-03-31 15:54:34 +0900

    fix: @focusが取得できるか検証
    BootStrapVueは読み込みがうまくいかない。TSにしなきゃよかったかも。

---
+++ vue/sandbox-bootstrap-vue/src/components/SandBoxForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <input @focus="logs" />
+</template>
+<script>
+import { Component, Vue } from "vue-property-decorator";
+
+Component;
+export default class SandBoxForm extends Vue {
+  logs() {
+    console.log("test");
+  }
+}
+</script>

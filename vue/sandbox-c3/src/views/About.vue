3aafbd7bf68469806a9ade5ad5f42764b2f0247d
MofuMofu <froakie002@gmail.com>
2021-04-07 12:03:51 +0900

    add page

--- vue/sandbox-c3/src/views/About.vue
+++ vue/sandbox-c3/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="c3-mock">
+    <h1>This is an c3 graph mock.</h1>
+    <Graph/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Graph from '@/components/Graph.vue'
+
+export default {
+  name: 'About',
+  components: {
+    Graph
+  }
+}
+</script>

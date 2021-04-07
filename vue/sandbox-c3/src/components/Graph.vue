d6ada795b097d00f53bc5aeee317f795d6371d1e
MofuMofu <froakie002@gmail.com>
2021-04-07 13:31:52 +0900

    add: add c3 graph

--- vue/sandbox-c3/src/components/Graph.vue
+++ vue/sandbox-c3/src/components/Graph.vue
@@ -1,12 +1,25 @@
 <template>
-  <div class="hello">
-    updated
+  <div class="c3-mock">
+    <div id="chart"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import c3 from 'c3'
 
 @Component
-export default class Graph extends Vue {}
+export default class Graph extends Vue {
+  get chart() {
+    return c3.generate({
+      bindto: '#chart',
+      data: {
+        columns: [
+          ['data1', 30, 200, 100, 400, 150, 250],
+          ['data2', 50, 20, 10, 40, 15, 25]
+        ]
+      }
+    })
+  }
+}
 </script>

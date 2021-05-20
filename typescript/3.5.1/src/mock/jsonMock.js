a7a263e692432532b8bbe48f959fecb7dcafae35
MofuMofu <froakie002@gmail.com>
2021-05-20 18:38:31 +0900

    joinを試してみる

---
+++ typescript/3.5.1/src/mock/jsonMock.js
@@ -0,0 +1,29 @@
+"use strict";
+exports.__esModule = true;
+exports.sortedEndwithE = exports.sortedTopping = exports.cookieTopping = void 0;
+exports.cookieTopping = [
+    { id: "5001", type: "None" },
+    { id: "5002", type: "Glazed" },
+    { id: "5005", type: "Sugar" },
+    { id: "5007", type: "Powdered Sugar" },
+    { id: "5003", type: "Chocolate" },
+    { id: "5004", type: "Maple" },
+];
+exports.sortedTopping = [
+    { id: "5003", type: "Chocolate" },
+    { id: "5002", type: "Glazed" },
+    { id: "5004", type: "Maple" },
+    { id: "5001", type: "None" },
+    { id: "5007", type: "Powdered Sugar" },
+    { id: "5005", type: "Sugar" },
+];
+exports.sortedEndwithE = [
+    { id: "5003", type: "Chocolate" },
+    { id: "5004", type: "Maple" },
+    { id: "5001", type: "None" },
+    { id: "5002", type: "Glazed" },
+    { id: "5007", type: "Powdered Sugar" },
+    { id: "5005", type: "Sugar" },
+];
+// Source
+// https://opensource.adobe.com/Spry/samples/data_region/JSONDataSetSample.html#Example4
--- typescript/3.5.1/src/practiceObjectOperation.ts
+++ typescript/3.5.1/src/practiceObjectOperation.ts
@@ -2,9 +2,23 @@
 
 import {cookieTopping} from '../src/mock/jsonMock'
 
-// [ '0', { id: '5001', type: 'None' } ],のように、[index番号 + オブジェクト]の配列がキーの数だけ生成される
+const object1 = {
+  A: 'a',
+  B: 'b'
+}
+
 function objectEntries(param: Object) {
   return Object.entries(param)
 }
 
-console.log(objectEntries(cookieTopping))
+// [ [ 'A', 'a' ], [ 'B', 'b' ] ]
+// console.log(objectEntries(object1))
+
+function joinParams(param: Object) {
+  return Object.entries(param).join('-')
+}
+
+// A,a-B,b
+// console.log(joinParams(object1))
+ 
+
